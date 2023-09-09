5d14a36faae1fc1a56adb2c3b9ff8a4d91bea1ee
qwe qwe <krupovroman@gmail.com>
2023-09-09 04:06:09 +0300

    исправлено падение производительности при отрисовке большого количества карточек

--- src/components/MnemCard.vue
+++ src/components/MnemCard.vue
@@ -35,13 +35,15 @@
 <template>
     <div class="mnem-box">
         <div class="mnem-box-group-name"><span style="max-width: 80%; text-decoration: underline;">{{ groupName ? groupName : ' - ' }}</span>
-          <n-button v-if="isChrome" circle tertiary :onclick="() => { playVoice(wordWithKanji) }">
-            <template #icon>
-              <n-icon>
+          <div class="player" :onclick="() => { playVoice(wordWithKanji) }"><span>&#9658;</span></div>
+          <!-- <div class="player"><span></span></div> -->
+          <!-- <n-button v-if="isChrome" circle tertiary :onclick="() => { playVoice(wordWithKanji) }"> -->
+            <!-- <template #icon> -->
+              <!-- <n-icon>
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512"><path d="M133 440a35.37 35.37 0 0 1-17.5-4.67c-12-6.8-19.46-20-19.46-34.33V111c0-14.37 7.46-27.53 19.46-34.33a35.13 35.13 0 0 1 35.77.45l247.85 148.36a36 36 0 0 1 0 61l-247.89 148.4A35.5 35.5 0 0 1 133 440z" fill="currentColor"></path></svg>
-              </n-icon>
-            </template>
-          </n-button>
+              </n-icon> -->
+            <!-- </template> -->
+          <!-- </n-button> -->
         </div>
         <div class="mnem-box-text"><span>{{ wordWithKanji }}</span></div>
         <div class="mnem-box-text"><span>{{ word ? word : ' - ' }}</span></div>
@@ -88,4 +90,31 @@
     background-color: white;
     border-radius: 50%;
 }
+.player {
+  background: none;
+  width: 25px;
+  height: 25px;
+  border: solid 2px #fff;
+  border-radius: 100%;
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.player span {
+  height: 25px;
+  margin-top: 4px;
+  color: #fff;
+  margin-left: 2px
+}
+.player:hover {
+  border: solid 2px #C08916;
+}
+
+.player:hover span {
+  color: #C08916;
+}
+
 </style>
